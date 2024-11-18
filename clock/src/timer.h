983b52c840522f518c820b67c1eff9e0d090b274
Stephen Sherratt <stephen@sherra.tt>
2024-11-19 01:09:52 +1100

    Update clock schematic

--- clock/src/timer.h
+++ clock/src/timer.h
@@ -1,5 +1,10 @@
 #include <stdint.h>
 
+/* This uses the 16-bit timer in "normal mode" where it continuously counts
+ * from 0 to 0xFFFF, wrapping around to 0. It runs at clkIO/1024. The expected
+ * use-case is to reset the clock and then poll the `timer_read` function to
+ * monitor the passage of time. */
+
 void timer_init(void);
 void timer_reset(void);
 uint16_t timer_read(void);
